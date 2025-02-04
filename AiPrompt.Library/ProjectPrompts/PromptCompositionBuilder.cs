64ac5c4798c4989593c082224b1fa35d0fcdcb6c
Stewart Scott Rogers <Stewart.Rogers@POBox.com>
2025-02-04 08:20:49 -0700

    Add new PromptCompositionBuilder and update project prompts
    Added PromptCompositionBuilder.cs to AiPrompt.Library.projitems.
    Updated various AI prompt project descriptions to focus on specific
    application implementations. Added LMStudio.Generated.SolutionPrompts
    project to LMStudio.API.sln. Created new project items and shared
    project files for LMStudio.Generated.SolutionPrompts.

---
+++ AiPrompt.Library/ProjectPrompts/PromptCompositionBuilder.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AiPrompt.Library.ProjectPrompts
+{
+    public static class PromptCompositionBuilder
+    {
+    }
+}
